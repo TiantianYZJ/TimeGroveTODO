9ebacc4c0b8883056b1e76a27ef415080f77f36b
TiantianYZJ <yzjtiantian@126.com>
2026-07-25 16:42:25 +0800

    fix: 维护遮罩改为点击图标5次隐藏，去掉关闭按钮

--- pages/community-home/community-home.js
+++ pages/community-home/community-home.js
@@ -286,6 +286,15 @@ Page({
     this.setData({ showMaintenanceOverlay: false });
   },
 
+  onMaintenanceTap() {
+    if (!this.data._dismissCount) this.data._dismissCount = 0;
+    this.data._dismissCount++;
+    if (this.data._dismissCount >= 5) {
+      this.data._dismissCount = 0;
+      this.dismissMaintenance();
+    }
+  },
+
   _getWeekMonths() {
     const { checkinApi } = require('../../utils/api');
     const today = new Date();
