6b5588eabd24cf03cfb3577c018db4a34e56eb49
TiantianYZJ <yzjtiantian@126.com>
2026-07-27 14:19:02 +0800

    fix: replace full post.files array in observer to trigger re-render

--- components/post-card/post-card.js
+++ components/post-card/post-card.js
@@ -26,17 +26,14 @@ Component({
     },
     'post'(post) {
       if (post && post.files && post.files.length > 0) {
-        const iconMap = {};
-        post.files.forEach((f, i) => {
+        const enriched = post.files.map((f, i) => {
           const icon = this.getFileIcon(f.mime_type || f.content_type, f.filename);
           const isExpired = this.isFileExpired && this.isFileExpired(f.expires_at);
           const days = this.getFileRemainingDays ? this.getFileRemainingDays(f.expires_at) : '';
           console.log('[post-card] file['+i+']:', f.filename, 'icon:', icon, 'expires_at:', f.expires_at, 'isExpired:', isExpired, 'days:', days);
-          iconMap['post.files['+i+']._icon'] = icon;
-          iconMap['post.files['+i+']._remainingDays'] = String(days);
-          iconMap['post.files['+i+']._isExpired'] = isExpired;
+          return { ...f, _icon: icon, _remainingDays: String(days), _isExpired: isExpired };
         });
-        this.setData(iconMap);
+        this.setData({ 'post.files': enriched });
       }
     },
   },
